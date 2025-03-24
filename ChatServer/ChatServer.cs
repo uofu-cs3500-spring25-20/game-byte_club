f8d04016fca8590df1d2ba8a65e7848761dcf8e4
Lexia Ramirez <lexiaramirez13@gmail.com>
2025-03-24 13:11:27 -0600

    start

---
+++ ChatServer/ChatServer.cs
@@ -0,0 +1,61 @@
+﻿// <copyright file="ChatServer.cs" company="UofU-CS3500">
+// Copyright (c) 2024 UofU-CS3500. All rights reserved.
+// </copyright>
+
+using CS3500.Networking;
+using System.Net.Sockets;
+using System.Text;
+
+namespace CS3500.Chatting;
+
+/// <summary>
+///   A simple ChatServer that handles clients separately and replies with a static message.
+/// </summary>
+public partial class ChatServer
+{
+    private Dictionary<string, TcpClient> clients = new Dictionary<string, TcpClient>();
+    private int clientCount = 1;
+
+    /// <summary>
+    ///   The main program.
+    /// </summary>
+    /// <param name="args"> ignored. </param>
+    /// <returns> A Task. Not really used. </returns>
+    private static void Main( string[] args )
+    {
+        Server.StartServer( HandleConnect, 11_000 );
+        Console.Read(); // don't stop the program.
+    }
+
+
+    /// <summary>
+    ///   <pre>
+    ///     When a new connection is established, enter a loop that receives from and
+    ///     replies to a client.
+    ///   </pre>
+    /// </summary>
+    ///
+    private static void HandleConnect( NetworkConnection connection )
+    {
+        // handle all messages until disconnect.
+        try
+        {
+            while ( true )
+            {
+                var message = connection.ReadLine( );
+
+                foreach(TcpClient c in ChatServer.clients.Values)
+                {
+                    NetworkConnection nc = new NetworkConnection(c);
+                    nc.Send(message);
+                }
+            }
+        }
+        catch ( Exception )
+        {
+            // do anything necessary to handle a disconnected client in here
+        }
+    }
+
+
+}
